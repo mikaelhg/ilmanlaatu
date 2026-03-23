d5ffd30d4c71a56717ec5edfa17ed82efdcbdfe6
Mikael Gueck <gumi@iki.fi>
2026-03-23 23:25:05 +0200

    use relative paths

--- site/rsbuild.config.ts
+++ site/rsbuild.config.ts
@@ -3,23 +3,14 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:8080',
-    },
-  },
   html: {
-    title: 'Counter',
-    /*
-    tags: [
-      {
-        tag: 'link',
-        attrs: {
-          rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
-        },
-      },
-    ],
-    */
+    title: 'Ilmanlaatu Helsingissä',
+  },
+  output: {
+    assetPrefix: './',
+    distPath: {
+      js: '',
+      css: '',
+    },
   },
 });
