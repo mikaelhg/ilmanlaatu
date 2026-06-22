b2b03b6db181f1292fbefabc857b1ea3f621990e
Mikael Gueck <gumi@iki.fi>
2026-06-22 09:40:23 +0300

    minor react refactor

--- site/src/components/AirQualityGraph.tsx
+++ site/src/components/AirQualityGraph.tsx
@@ -1,14 +1,17 @@
+import { useMemo } from "react";
 import { VegaEmbed } from "react-vega";
 import { makeVegaSpec, vegaOptions } from "./aq-vega";
 
 interface AirQualityProps {
   field: string;
   title: string;
-  domainMax: number;
+  domainMax?: number;
 }
 
 export default function AirQualityGraph({ field, title, domainMax = 100 }: AirQualityProps) {
-  const spec = makeVegaSpec(field, title, domainMax);
+  const spec = useMemo(() => {
+    return makeVegaSpec(field, title, domainMax);
+  }, [field, title, domainMax]);
   return (
     <VegaEmbed className="h-full w-full" spec={spec} options={vegaOptions} />
   );
